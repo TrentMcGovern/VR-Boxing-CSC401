be574948490ac9a64a3896b3cdf8777ac426c030
Trent McGovern <trentmcgovern6@gmail.com>
2025-12-08 15:53:15 -0500

    added hit sfx

--- VR Boxing/Assets/Scripts/FruitVFX.cs
+++ VR Boxing/Assets/Scripts/FruitVFX.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FruitVFX : MonoBehaviour
 {
+
+
     public GameObject deathParticles;
     // Start is called before the first frame update
+
+    public AudioClip hitSound; // Assign in Inspector
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); // Gets the AudioSource on this GameObject
+    }
 
     void OnCollisionEnter(Collision collision)
     {
@@ -16,9 +27,11 @@ public class FruitVFX : MonoBehaviour
             // if (collision.gameObject.CompareTag("Enemy"))
             // {
             // Get the contact point and rotation for correct orientation
+            audioSource.PlayOneShot(hitSound, 0.5f);
             ContactPoint contact = collision.contacts[0];
             Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
             Vector3 position = contact.point;
+
 
             // Instantiate the particle effect at the collision point and rotation
             GameObject effectInstance = Instantiate(deathParticles, position, rotation);
