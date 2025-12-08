aac3f9c667b111940be619ab3d78d0ca7813ecc6
Trent McGovern <trentmcgovern6@gmail.com>
2025-12-08 15:07:03 -0500

    updated hit vfx
    the hit vfx on the fruit now only occur when hit by boxing gloves, not when it collides with anything

--- VR Boxing/Assets/Scripts/FruitVFX.cs
+++ VR Boxing/Assets/Scripts/FruitVFX.cs
@@ -9,17 +9,25 @@ public class FruitVFX : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision Detected");
-        // Optional: Check the tag of the object collided with
-        // if (collision.gameObject.CompareTag("Enemy"))
-        // {
-        // Get the contact point and rotation for correct orientation
-        ContactPoint contact = collision.contacts[0];
-        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
-        Vector3 position = contact.point;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision Detected");
+            // Optional: Check the tag of the object collided with
+            // if (collision.gameObject.CompareTag("Enemy"))
+            // {
+            // Get the contact point and rotation for correct orientation
+            ContactPoint contact = collision.contacts[0];
+            Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 position = contact.point;
 
-        // Instantiate the particle effect at the collision point and rotation
-        GameObject effectInstance = Instantiate(deathParticles, position, rotation);
+            // Instantiate the particle effect at the collision point and rotation
+            GameObject effectInstance = Instantiate(deathParticles, position, rotation);
+
+        }
 
     }
+
+
+
 }
+
