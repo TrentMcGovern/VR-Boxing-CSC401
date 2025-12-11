57dac75158149f2d8466b31ad29c80ca3d725e09
kay-dinh <42415211+kay-dinh@users.noreply.github.com>
2025-12-10 23:59:22 -0500

    public sliders

--- VR Boxing/Assets/Scripts/PauseMenu.cs
+++ VR Boxing/Assets/Scripts/PauseMenu.cs
@@ -1,28 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 
 public class PauseMenu : MonoBehaviour
 {
-    AudioSource m_MyAudioSource;
+    public AudioSource m_MyAudioSource;
     //Value from the slider, and it converts to volume level
-    float m_MySliderValue;
+    public float m_MySliderValue;
+    public GameObject pause;
+    public Slider volume;
+    public Slider sfx;
+    public Toggle snap_turn;
+
 
     void Start()
     {
         //Initiate the Slider value to half way
-        m_MySliderValue = 0.5f;
+        volume.value = 0.5f;
+        sfx.value = 0.5f;
         //Fetch the AudioSource from the GameObject
         m_MyAudioSource = GetComponent<AudioSource>();
         //Play the AudioClip attached to the AudioSource on startup
-        m_MyAudioSource.Play();
+        //m_MyAudioSource.Play();
     }
 
-    void OnGUI()
-    {
-        //Makes the volume of the Audio match the Slider value
-        m_MyAudioSource.volume = m_MySliderValue;
-    }
 }
 
