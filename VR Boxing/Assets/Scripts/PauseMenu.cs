b2a0f3ceddcf63e56f7edac69c9200aef3b9b62c
Trent McGovern <trentmcgovern6@gmail.com>
2025-12-11 10:10:24 -0500

    starting to implement ui into game, updated intro room a bit,  made player not get pushed around by fruit

--- VR Boxing/Assets/Scripts/PauseMenu.cs
+++ VR Boxing/Assets/Scripts/PauseMenu.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    private bool isPaused;
+
+    public InputActionProperty pauseButton;
     public AudioSource m_MyAudioSource;
     //Value from the slider, and it converts to volume level
     public float m_MySliderValue;
-    public GameObject pause;
+   
     public Slider volume;
     public Slider sfx;
     public Toggle snap_turn;
 
+    void Awake()
+    {
+        if(pauseMenu != null)
+        {
+            pauseMenu.SetActive(false);
+        }
+    }
 
+    private void OnEnable()
+    {
+        pauseButton.action.performed += OnPauseAction; 
+    }
     void Start()
     {
         //Initiate the Slider value to half way
@@ -26,6 +42,35 @@ public class PauseMenu : MonoBehaviour
         m_MyAudioSource = GetComponent<AudioSource>();
         //Play the AudioClip attached to the AudioSource on startup
         //m_MyAudioSource.Play();
+    }
+
+    private void OnPauseAction(InputAction.CallbackContext context)
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+    public void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0f; // Pauses most game physics and movement
+        pauseMenu.SetActive(true); // Show the menu
+
+        // You may need additional code here to manage the VR cursor visibility 
+        // or switch hand models (as Half-Life Alyx does)
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1f; // Resumes game time
+        pauseMenu.SetActive(false); // Hide the menu
+        // Re-hide the cursor if necessary
     }
 
 }
--- VR Boxing/Assets/Scripts/PlayerSounds.cs
+++ VR Boxing/Assets/Scripts/PlayerSounds.cs
@@ -6,15 +6,18 @@ public class PlayerSounds : MonoBehaviour
 {
     [SerializeField]
     private AudioClip hitSound; // Assign in Inspector
+    [SerializeField]
     AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>(); // Gets the AudioSource on this GameObject
+        //audioSource = GetComponent<AudioSource>(); // Gets the AudioSource on this GameObject
     }
+
 
     void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Collided with: " + collision.gameObject.name);
         if (collision.gameObject.CompareTag("Fruit"))
         {
             audioSource.PlayOneShot(hitSound);
